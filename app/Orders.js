71092617ef7644c0747c587bdc4ab03410b63d09
Uwumukiza12 <marcelresist12@gmail.com>
2021-10-27 10:50:16 +0200

    orders modified

--- app/Orders.js
+++ app/Orders.js
@@ -1,50 +1,62 @@
 import React, {useEffect, useState} from 'react'
-import {ActivityIndicator, FlatList,Text, View, Button, TextInput} from 'react-native';
-import axios from 'axios';
-function Orders(props) {
+import {ActivityIndicator, Text, View, Button, TextInput, ScrollView} from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
+import Axios from 'axios';
+function Orders() {
     const [isLoading, setLoading]= useState(true);
     const [data, setData] = useState([]);
     //console.log('Hello error')
-    const getMovies = async () => {
-        try {
-        const response= await axios.get('http://192.168.137.121:3000/posts')
+    //axios
+    Axios({
+      method: "GET",
+      baseURL:
+        "http://192.168.137.121:3000/posts",
+    })
+      .then((res) =>{ setData(res)
+      alert(res)
+      })
+      .catch((err) => console.error(err));
+
+    //##################################################
+    // const getMovies = async () => {
+    //     try {
+    //     const response= await axios.get('http://192.168.137.121:3000/posts')
         
-        // .then((data)=>{
-        //   console.log(JSON.stringify(response.data));
-        // setData(data);
-        //  })
+    //     // .then((data)=>{
+    //     //   console.log(JSON.stringify(response.data));
+    //     // setData(data);
+    //     //  })
       
 
-        alert(JSON.stringify(response.data));
-         //console.log('Hello error')
-         //const json = await response.json();
-        //setData(json.posts);
-        // console.log('Hello error')
-       } catch (error) {
-         console.error(error);
-       } finally {
-         setLoading(false);
-       }
-     }
-    useEffect(()=>{
-        getMovies();
-    }, []);
+    //     alert(JSON.stringify(response.data));
+    //      //console.log('Hello error')
+    //      //const json = await response.json();
+    //     //setData(json.posts);
+    //     // console.log('Hello error')
+    //    } catch (error) {
+    //      console.error(error);
+    //    } finally {
+    //      setLoading(false);
+    //    }
+    //  }
+    // useEffect(()=>{
+    //     getMovies();
+    // }, []);
+    //########################################################
     return (
         <>
-        <View style={{ flex: 1, padding: 24 }}>
-            {isLoading ? <ActivityIndicator/> : (
+        <View style={{ flex: 1, padding: 24,  }}>
+            
               <FlatList
-                data={data}
-                keyExtractor={({ id }) => id}
-                renderItem={({ item }) => (
-                  <Text>{item.title}, {item.description}</Text>
-                )}
-              />
-            )}
+              data={data}
+              keyExtractor={(id) => id.toString()}
+              renderItem={({ item }) => <Text>{item.title} hello</Text>}
+            />
+            
 
-          <Button
+          {/* <Button
                 title="Fetch data" onPress={getMovies}
-                />
+                /> */}
           </View>
     
            
